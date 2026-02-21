8b18be7f9639f8e9584eab6390bab7450c1d8d91
Hunter Kit Sabio <hunterkit.campos.sabio@gmail.com>
2026-02-21 23:23:20 +0800

    Add authentications

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+const cookieSession = require('cookie-session');
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cookieSession({
+    keys: ['hhkjh643h5jkh5kjh2k5jh34kjhkjh634jkh64jkh6'],
+    maxAge: 24 * 60 * 60 * 1000 // 24 hours
+  }));
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true, // Automatically remove properties that do not have any decorators
